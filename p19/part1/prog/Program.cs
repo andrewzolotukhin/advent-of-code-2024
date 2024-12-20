a6f5d2eeae4ec8a83a381728cbf43a2b952a72c0
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2024-12-20 03:15:18 +0200

    P19: accepted

---
+++ p19/part1/prog/Program.cs
@@ -0,0 +1,52 @@
+﻿string prefixesStr = Console.ReadLine()!;
+
+var prefixes = prefixesStr.Split(", ", 
+    StringSplitOptions.RemoveEmptyEntries)
+.ToDictionary(t => t, t => true);
+
+var maxKeyLength = prefixes.Keys.Max(t => t.Length);
+
+List<string> input = [];
+
+// read until EOF
+while (true)
+{
+    string? line = Console.ReadLine();
+    if (line == null)
+    {
+        break;
+    }
+    if(!string.IsNullOrEmpty(line)) {
+      input.Add(line);
+    }
+}
+
+long result = 0;
+
+
+foreach(var s in input) {
+  if(Helper.TryMatch(s, prefixes)) {
+    result++;
+  }
+}
+
+Console.WriteLine(result);
+
+internal static class Helper {
+  public static Dictionary<string, bool> dp = [];
+
+  public static bool TryMatch(string s, Dictionary<string, bool> prefixes) {
+  if(s == string.Empty) return true;
+  if(dp.ContainsKey(s)) return dp[s];
+  foreach(var key in prefixes.Keys) {
+    if(s.StartsWith(key)) {
+      if(TryMatch(s.Substring(key.Length), prefixes)) {
+        dp.Add(s, true);
+        return true;
+      }
+    }
+  }
+        dp.Add(s, false);
+  return false;
+}
+}
---
+++ p19/part2/prog/Program.cs
@@ -0,0 +1,55 @@
+﻿string prefixesStr = Console.ReadLine()!;
+
+var prefixes = prefixesStr.Split(", ", 
+    StringSplitOptions.RemoveEmptyEntries)
+.ToDictionary(t => t, t => true);
+
+var maxKeyLength = prefixes.Keys.Max(t => t.Length);
+
+List<string> input = [];
+
+// read until EOF
+while (true)
+{
+    string? line = Console.ReadLine();
+    if (line == null)
+    {
+        break;
+    }
+    if(!string.IsNullOrEmpty(line)) {
+      input.Add(line);
+    }
+}
+
+long result = 0;
+
+
+foreach(var s in input) {
+  result += Helper.TryMatch(s, prefixes);
+  Console.WriteLine($"after \"{s}\": {result}");
+}
+
+Console.WriteLine(result);
+
+internal static class Helper {
+  public static Dictionary<string, long> dp = [];
+
+  public static long TryMatch(string s, Dictionary<string, bool> prefixes) {
+  if(s == string.Empty)  {
+    return 1;
+  };
+  if(dp.ContainsKey(s)) {
+    return dp[s]; 
+  } 
+
+  var lresult = 0L;
+
+  foreach(var key in prefixes.Keys) {
+    if(s.StartsWith(key)) {
+      lresult+= TryMatch(s.Substring(key.Length), prefixes);
+    }
+  }
+  dp.TryAdd(s, lresult);
+  return lresult;
+}
+}
