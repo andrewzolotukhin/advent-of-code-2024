0b43bac2339b5e7d0a6e94e22fc665299879f0f0
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2024-12-26 01:56:28 +0200

    p23: accepted

---
+++ p23/part1/Program.cs
@@ -0,0 +1,61 @@
+﻿
+var graph = new Dictionary<string, List<string>>();
+
+// read until EOF
+while (Console.In.Peek() != -1)
+{
+    // read a line
+    string line = Console.ReadLine()!;
+    // write the line to the console
+    
+    var splitted = line.Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+    if(graph.ContainsKey(splitted[0]))
+    {
+        graph[splitted[0]].Add(splitted[1]);
+    }
+    else
+    {
+        graph.Add(splitted[0], [splitted[1]]);
+    }
+
+    if(graph.ContainsKey(splitted[1]))
+    {
+        graph[splitted[1]].Add(splitted[0]);
+    }
+    else
+    {
+        graph.Add(splitted[1], [splitted[0]]);
+    }
+}
+
+HashSet<string> result = new HashSet<string>();
+
+foreach(var vertex1 in graph.Keys)
+{
+    foreach(var vertex2 in graph[vertex1])
+    {
+        if(vertex1 == vertex2)
+        {
+            continue;
+        }
+        foreach(var vertex3 in graph[vertex2])
+        {
+            if(vertex3 == vertex1 || vertex3 == vertex2)
+            {
+                continue;
+            }
+
+            // if they are connected -> add to result
+            if(graph[vertex3].Contains(vertex1) && (vertex1.StartsWith('t') || vertex2.StartsWith('t') || vertex3.StartsWith('t')))
+            {
+                var keys = ((List<string>)[vertex1, vertex2, vertex3]);
+                keys.Sort();
+                result.Add(string.Join("-", keys));
+            }
+        }
+    }
+}
+
+Console.WriteLine(result.Count);
+
---
+++ p23/part2/Program.cs
@@ -0,0 +1,65 @@
+﻿var graph = new Dictionary<string, HashSet<string>>();
+
+// read until EOF
+while (Console.In.Peek() != -1)
+{
+    // read a line
+    string line = Console.ReadLine()!;
+    // write the line to the console
+    
+    var splitted = line.Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+    if(graph.ContainsKey(splitted[0]))
+    {
+        graph[splitted[0]].Add(splitted[1]);
+    }
+    else
+    {
+        graph.Add(splitted[0], [splitted[1]]);
+    }
+
+    if(graph.ContainsKey(splitted[1]))
+    {
+        graph[splitted[1]].Add(splitted[0]);
+    }
+    else
+    {
+        graph.Add(splitted[1], [splitted[0]]);
+    }
+}
+
+
+int maxClique = 0;
+var result = "";
+
+void FindMaxClique(HashSet<string> current, HashSet<string> candidates, HashSet<string> others, ref int max) {
+  if (candidates.Count == 0 && others.Count == 0) {
+    if (current.Count > max) {
+      max = current.Count;
+      result = string.Join(",", current.Order());
+    }
+    return;
+  }
+
+  foreach (var candidate in candidates) {
+    var newCurrent = new HashSet<string>(current);
+    newCurrent.Add(candidate);
+
+    var newCandidates = new HashSet<string>(candidates);
+    newCandidates.IntersectWith(graph[candidate]);
+
+    var newOthers = new HashSet<string>(others);
+    newOthers.IntersectWith(graph[candidate]);
+
+    FindMaxClique(newCurrent, newCandidates, newOthers, ref max);
+
+    candidates.Remove(candidate);
+    others.Add(candidate);
+  }
+}
+
+FindMaxClique(new HashSet<string>(), new HashSet<string>(graph.Keys), new HashSet<string>(), ref maxClique);
+
+Console.WriteLine(maxClique);
+Console.WriteLine(result);
+
